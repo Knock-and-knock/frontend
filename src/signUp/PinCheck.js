2996560bc02bc73a0f81261a18a2fb67d0d14358
dlenfl0924 <dlenfl0924@naver.com>
2024-08-19 08:53:44 +0900

    [Feat]: 회원가입페이지 퍼블리싱 시작

---
+++ src/signUp/PinCheck.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignUpHeader from './component/header/SignUpHeader';
+
+function PinCheck(props) {
+    return (
+        <div>
+            <SignUpHeader/>
+        </div>
+    );
+}
+
+export default PinCheck;
