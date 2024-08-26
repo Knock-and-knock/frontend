91cb2668efab03e4f3d907d665823614e8f1c7a3
sondonghee123 <ghost1229@naver.com>
2024-08-26 17:36:34 +0900

    [Refactor]: 혹시 몰라 커밋해요

---
+++ android/app/src/main/java/com/localhost/knockknock/MainActivity.java
@@ -0,0 +1,5 @@
+package com.localhost.knockknock;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.localhost.knockknock',
+  appName: 'Knock Knock',
+  webDir: 'build'
+};
+
+export default config;
